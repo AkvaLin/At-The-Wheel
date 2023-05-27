9653dbf899fda459fbc4c060132d870968d6717f
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-05-27 11:45:16 +0300

    add: player movement

--- Project/game.py
+++ Project/game.py
@@ -1,6 +1,8 @@
 import pygame
+from steeringwheel import steering
 from world import World
 from services import blit_rotate
+from player import Player
 from pygame.locals import (
     K_ESCAPE,
     KEYDOWN,
@@ -22,11 +24,24 @@ clock = pygame.time.Clock()
 world = World()
 world_width, world_height = world.surf.get_size()
 
+player_x_default = 415
+player_x_max = 510
+player_x_min = 320
+
+player_x = player_x_default
+player_y = 360
+
+player_offset = 5
+
+player = Player(3)
+
 angle = 0
+counter = 0
+turn = 0
 done = False
 while not done:
     clock.tick(60)
-
+    counter += 1
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -35,10 +50,21 @@ while not done:
         elif event.type == QUIT:
             done = True
 
+    if counter == 6:
+        turn = steering()
+        counter = 0
+
+    if turn == -1:
+        if player_x_min <= player_x - player_offset:
+            player_x -= player_offset
+    elif turn == 1:
+        if player_x_max >= player_x + player_offset:
+            player_x += player_offset
 
     pos = (screen.get_width() / 2, screen.get_height() / 2)
 
     blit_rotate(screen, world.surf, pos, (world_width / 2, world_height / 2), angle)
+    screen.blit(player.surf, (player_x, player_y))
     angle += 1.2
 
     pygame.display.flip()
